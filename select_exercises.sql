c6a00e30933e64984f998dcaef59d4279ec874a6
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-01-11 16:20:43 -0600

    All albums released in the 1990s

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,10 @@ SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind' AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'Info';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
