5a1595bc60579dec5e5995ca15a90f6f1bd762ef
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-01-11 16:24:38 -0600

    Which albums had less than 20 million certified sales

--- select_exercises.sql
+++ select_exercises.sql
@@ -11,3 +11,5 @@ SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT 'Which albums were released in the 1990s' AS 'Info';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
+SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
+SELECT * FROM albums WHERE sales < 20;
