946a95db0a69c704f6e7d559dd615440ac30186e
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-01-11 16:55:01 -0600

    Move all albums before 1980 back to the 1800s

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,3 +3,9 @@ SELECT 'Make all the albums 10 times more popular' AS 'Info';
 SELECT * FROM albums;
 UPDATE albums
 SET sales = sales * 10;
+
+SELECT 'Move all the albums before 1980 back to the 1800s.' AS 'Info';
+SELECT * FROM albums WHERE release_date <= 1980;
+UPDATE albums
+SET release_date = 1800
+WHERE release_date <= 1980;
