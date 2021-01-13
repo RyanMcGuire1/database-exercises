0a273dd99c5c8498d55f86f607ffc3dd4fb90b39
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-01-13 10:26:41 -0600

    Order By exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,20 +3,22 @@ USE employees;
 SELECT *
 FROM employees
 WHERE (first_name = 'Irena'
-        OR first_name = 'Vidya'
-        OR first_name = 'Maya')
-  AND gender = 'M';
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'M'
+  ORDER BY last_name DESC , first_name;
 
 
 SELECT *
 FROM employees
 WHERE last_name LIKE '%E'
-OR last_name LIKE 'E%';
+   OR last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE '%E'
-AND last_name LIKE 'E%';
+  AND last_name LIKE 'E%';
 
 SELECT *
 FROM employees
@@ -25,7 +27,8 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25%'
-AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31');
+  AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC;
 
 SELECT *
 FROM employees
